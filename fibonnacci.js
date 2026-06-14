a61d7e2ad4c65d3c2ececf13e91d0f26af643719
khushi1911 <khushikaushik2030@gmail.com>
2026-06-14 22:52:53 +0530

    fibonnacci code

---
+++ fibonnacci.js
@@ -0,0 +1,24 @@
+function fibonacciGenerator (n) {
+//Do NOT change any of the code above 👆
+    var arr = [];
+    //Write your code here:
+    if(n === 1){
+        arr = [0];
+    }else if(n === 2){
+        arr = [0,1];
+    }else{
+        arr = [0,1];
+        for(var i=2; i<n ;i++){
+           arr.push(arr[arr.length-2]+arr[arr.length-1]); 
+        }   
+    }
+    
+    return arr;
+    
+    
+    
+    //Return an array of fibonacci numbers starting from 0.
+    
+//Do NOT change any of the code below 👇
+}
+
