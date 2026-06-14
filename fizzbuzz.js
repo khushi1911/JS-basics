b0e0c2557e7a051e287331f9663ac8e10a5f86c1
khushi1911 <khushikaushik2030@gmail.com>
2026-06-14 19:14:24 +0530

    fizz buzz code correct

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,7 +2,7 @@ var output =[];
 var count = 1;
 
 
-function addElem(count){
+function addElem(){
 
 
 if(count % 3 === 0){
@@ -14,10 +14,10 @@ else if (count % 5===0){
     output.push("FizzBuzz");
 }else{
     output.push(count);
-    count++;
+    
 }
-
+count++;
 console.log(output);
 }
 
-addElem(count);
+addElem();
