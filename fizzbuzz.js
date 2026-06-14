327f9b4581cb9bd2b2213a2521a2a1d43948b6c8
khushi1911 <khushikaushik2030@gmail.com>
2026-06-14 19:16:47 +0530

    final

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,13 +5,12 @@ var count = 1;
 function addElem(){
 
 
-if(count % 3 === 0){
-    output.push("Fizz");
-}
-else if (count % 5===0){
-    output.push("Buzz");
-}else if (count % 5===0 && count % 3===0){
+if(count%5===0 && count%3===0){
     output.push("FizzBuzz");
+}else if (count%3===0){
+    output.push("Fizz");
+}else if (count%5===0){
+    output.push("Buzz");
 }else{
     output.push(count);
     
