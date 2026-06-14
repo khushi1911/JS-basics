b504a72c9bc49ff17ca10ed709e9561b6c721c38
khushi1911 <khushikaushik2030@gmail.com>
2026-06-14 17:07:20 +0530

    random no use

---
+++ randomNo.js
@@ -0,0 +1,8 @@
+prompt ("enter ur name");
+prompt("enter other person name");
+
+var lovepercent = Math.random() ;
+lovepercent = lovepercent *100 ;
+lovepercent = Math.floor(lovepercent) +1;
+
+alert("your love score is "+lovepercent+" %");
