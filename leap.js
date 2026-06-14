8b016c601af3823a45081085ac427b415ca20287
khushi1911 <khushikaushik2030@gmail.com>
2026-06-14 17:59:31 +0530

    leap year calculation

---
+++ leap.js
@@ -0,0 +1,12 @@
+function isLeap(year) {
+    
+    if (year%4==0){
+        return "Leap year.";
+    }else if( year%100 == 0){
+        return "Leap year.";
+    }else if (year %400 == 0){
+        return "Leap year.";
+    }else {
+        return "Not leap year.";
+    }   
+}
