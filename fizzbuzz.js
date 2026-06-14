6a584a5026fd0040403be8524fea821a5bb70daa
khushi1911 <khushikaushik2030@gmail.com>
2026-06-14 19:11:23 +0530

    fizz buzz code

---
+++ fizzbuzz.js
@@ -0,0 +1,23 @@
+var output =[];
+var count = 1;
+
+
+function addElem(count){
+
+
+if(count % 3 === 0){
+    output.push("Fizz");
+}
+else if (count % 5===0){
+    output.push("Buzz");
+}else if (count % 5===0 && count % 3===0){
+    output.push("FizzBuzz");
+}else{
+    output.push(count);
+    count++;
+}
+
+console.log(output);
+}
+
+addElem(count);
